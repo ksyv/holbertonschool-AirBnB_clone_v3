73109ffe4648959337bc70dea3cb784b151194a5
ksyv <viais.kevin@hotmail.fr>
2024-04-18 23:03:06 +0200

    fix import oorder

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 """Blueprint for the API"""
-
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,6 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
